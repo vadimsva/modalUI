f02e71c50336e06b2c902391b978da4b981f61cf
vadimsva <vadimsva@gmail.com>
2016-03-01 16:23:41 +0600

    01.03.16

--- modalUI.js
+++ modalUI.js
@@ -1,5 +1,5 @@
 /*
-modalUI - 1.06 beta [16.11.15]
+modalUI - 1.07 beta [01.03.16]
 Author: vadimsva
 Github: https://github.com/vadimsva/modalUI
 */
@@ -11,7 +11,17 @@ $(function(){
 	
 		var methods = {
 			init : function(params) {
-
+				
+				$('.tabs_container').each(function(){
+					if ($(this).hasClass('slider')) {
+						var active_tab = $(this).find('> .tab:first-child');
+						if ($(this).find('> .tab_slider').length == 0) {
+							$(this).append($('<div class="tab_slider"></div>'));
+						}
+						$(this).find('> .tab_slider').css({left:active_tab.position().left + parseInt(active_tab.css('marginLeft')) + 'px',width: active_tab.outerWidth() + 'px'});
+					}
+				});
+				
 				$('.tab').click(function() {
 					var curPage = $(this).attr('data-tab');
 					$(this).parent().find('> .tab').each(function(){
@@ -21,6 +31,7 @@ $(function(){
 						$(this).removeClass('active');
 					});
 					$(this).addClass('active');
+					$(this).parent().find('> .tab_slider').css({left:$(this).position().left + parseInt($(this).css('marginLeft')) + 'px',width: $(this).outerWidth() + 'px'});
 					$(this).parent().find('+ .page_container > .page[data-page="' + curPage + '"]').addClass('active');
 				});
 			
@@ -87,13 +98,11 @@ $(function(){
 		var methods = {
 			init : function(params) {
 				if (elem.find('> .progress_bar').length == 0) {
-					var prg_container = $('<div class="progress">');
-					var prg_text = $('<div class="progress_text"><div></div></div>');
-					var prg_bar = $('<div class="progress_bar" style="width:0%"><div></div></div>');
+					var prg_container = $('<div class="progress"></div>');
+					var prg_text = $('<div class="progress_text"><div style="width:'+ elem.outerWidth()+'px"></div></div>');
+					var prg_bar = $('<div class="progress_bar" style="width:0%"><div style="width:'+ elem.outerWidth()+'px"></div></div>');
 					prg_container.append(prg_text, prg_bar);
 					elem.append(prg_container);
-					elem.find('> .progress > .progress_text > div').css({width: elem.outerWidth() + 'px'});
-					elem.find('> .progress > .progress_bar > div').css({width: elem.outerWidth() + 'px'});
 				}
 			},
 			update : function(params) {
@@ -178,7 +187,99 @@ $(function(){
 })(jQuery);
 
 
+/* upload */
+(function($) {
+	$.fn.upload = function(method){
+		var methods = {
+			init : function(params) {
 
+				$('.upload input').change(function(){
+					var files = '';
+					for (var i = 0; i < $(this)[0].files.length; ++i) {
+						files += '<span title="'+$(this)[0].files[i].name+'">'+$(this)[0].files[i].name+'</span>';
+					}
+					$(this).parent().find('+ .uploaded_files').html(files);
+				});
+			
+			}
+		};
+
+		if (methods[method]) {
+			return methods[method].apply( this, Array.prototype.slice.call(arguments, 1));
+		} else if (typeof method === 'object' || ! method) {
+			return methods.init.apply(this, arguments);
+		}
+		
+	};
+	$('html').upload();
+})(jQuery);
+
+
+
+
+
+
+
+/* wave */
+(function($) {
+	$.fn.wave = function(method){
+		return this.each(function() {
+		var elem = $(this),
+		animTime;
+	
+		var methods = {
+			init : function() {
+				
+				elem.click(function(event) {
+					var elemObj = $('<span class="wave-pop"></span>');
+					$(this).prepend(elemObj);
+					getAnimTime(elemObj);
+					var x = event.pageX - $(event.target).offset().left;
+					var y = event.pageY - $(event.target).offset().top;
+					k = (elem.outerWidth() - x)*2/elemObj.width();
+					if (x > elem.outerWidth()/2) {
+						k = x*2/elemObj.width();
+					}
+					if (elem.outerWidth() == elem.outerHeight()) {
+						k = k + 0.2;
+					}
+					elemObj.css({marginLeft:x+'px',marginTop:y+'px',transform:'scale('+k+')',opacity:1});
+					setTimeout(function () {
+						elemObj.css({opacity:0});
+						setTimeout(function () {
+							elemObj.remove();
+						}, animTime);
+					}, animTime);
+				});
+				
+				function getAnimTime(elemObj) {
+					if (!animTime) {
+						animTime = elemObj.css('transitionDuration');
+						if (animTime !== undefined) {
+							animTime = animTime.replace('s', '') * 1000;
+						} else {
+							animTime = 0;
+						}
+					}
+				}
+
+			}
+		};
+
+		if (methods[method]) {
+			return methods[method].apply( this, Array.prototype.slice.call(arguments, 1));
+		} else if (typeof method === 'object' || ! method) {
+			return methods.init.apply(this, arguments);
+		}
+		});
+	};
+	$('.wave').wave('init');
+	document.addEventListener("DOMNodeInserted", function(event) {
+		if ($(event.target).hasClass('wave')) {
+			$('.wave').wave('init');
+		}
+	}, false);
+})(jQuery);
 
 
 
